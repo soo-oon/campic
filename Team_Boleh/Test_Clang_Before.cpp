a704f9fa8c3b142a682693e3216b9ff2a1dcd05c
DESKTOP-TII3EMF\Student <jinhyun175@gmail.com>
2018-09-10 14:56:14 +0900

    clang update

---
+++ Team_Boleh/Test_Clang_Before.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class vector
+{
+	private:
+	int x;
+	int y;
+	
+	public:
+	vector() : x(0), y(0)
+	{}
+	vector(int x_, int y_) : x(x_) , y(y_)
+	{}
+	
+	void print_vector()
+	{
+		std::cout<< "x pos: "<<x<<", "<<"y pos: "<<y<<std::endl;
+	}
+};
+
+int main(void)
+{
+	vector a(5, 3);
+	vector b;
+	
+	a.print_vector();
+	b.print_vector();
+	
+	return 0;
+}
