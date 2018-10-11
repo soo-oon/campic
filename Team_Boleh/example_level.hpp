dce01327583e1a29d11d9e0a793c24281483eae7
DESKTOP-DH54MFI\user <jinhyun.choi@digipen.edu>
2018-10-11 23:48:22 +0900

    prototype for engineproof

--- Team_Boleh/example_level.hpp
+++ Team_Boleh/example_level.hpp
@@ -7,6 +7,7 @@
 class example : public State
 {
 public:
+	void blackhole(Object* Ob);
     void move_enemy(float dt, Object* Ob);
     bool Initialize() override;
     void Update(float dt) override;
@@ -16,6 +17,7 @@ private:
 	float pm = 1;
 	float limit_time = 0;
 	float rotation_value = 10;
+	vector2 direction = 0;
     std::vector<vector2> opponent;
     std::vector<vector2> mesh_p;
     std::vector<vector2> static_opponent;
